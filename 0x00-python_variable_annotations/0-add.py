0e36ebc2e540e76bbaf861ca9abfa72c52a93830
Odarell35 <leratomawash0@gmail.com>
2024-03-14 20:24:59 +0200

    files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Addition"""
+
+
+def add(x: float, y: float) -> float:
+    """Add two floating-point numbers"""
+    return x + y
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""concatenation"""
+
+
+def concat(s1: str, s2: str) -> str:
+    """Concatenate two strings and return the result."""
+    return s1 + s2
