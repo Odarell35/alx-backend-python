d665cd49d947a002f17d134519cc7f22b53704bb
Odarell35 <leratomawasha0@gmail.com>
2024-03-22 11:34:48 +0200

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """module docs"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
+
