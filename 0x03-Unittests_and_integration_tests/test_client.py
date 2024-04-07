dade8e6bc4a7cb051d5d6a1c1797ae2b09981d70
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-07 15:13:50 +0200

    testing public repos methods

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -39,3 +39,18 @@ class TestGithubOrgClient(unittest.TestCase):
     org_client = GithubOrgClient("exampleorg")
     expected_url = "https://api.github.com/orgs/exampleorg/repos"
     self.assertEqual(org_client._public_repos_url, expected_url)
+
+  @patch('github_org_client.GithubOrgClient.get_json')
+  def test_public_repos(self, mock_get_json, mock_public_repos_url):
+    """test case method"""
+    mock_public_repos_url.return_value = "https://api.github.com/orgs/example_org/repos"
+    mock_payload = [{"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]
+    mock_get_json.return_value = mock_payload
+
+    github_org_client = GithubOrgClient()
+    repos = github_org_client.public_repos()
+    mock_public_repos_url.assert_called_once()
+    mock_get_json.assert_called_once()
+
+    expected_repos = ["repo1", "repo2", "repo3"]
+    self.assertEqual(repos, expected_repos)
