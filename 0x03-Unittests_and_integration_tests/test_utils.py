76f15e9102da8f6dcffbb8d8b8e99b042ddb4236
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-07 13:02:14 +0200

    pycodestyle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,49 +1,51 @@
 #!/usr/bin/env python3
 """Module"""
+
 import unittest
 from typing import Mapping, Sequence, Any
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from unittest.mock import patch, Mock
-from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
-  """class that inherits from unittest.TestCase"""
-  @parameterized.expand([
-      ({"a": 1}, ("a",), 1),
+    """class that inherits from unittest.TestCase"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-   def test_access_nested_map(self, nested_map, path, expected_output):
-     """testing function"""
-     result = access_nested_map(nested_map, path)
-     self.assertEqual(result, expected_output)
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """testing function"""
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
 
     @parameterized.expand([
         ({}, ("a",), 'a'),
         ({"a": 1}, ("a", "b"), 'b')
     ])
-  def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), expected_exception_message)
 
+
 class TestGetJson(unittest.TestCase):
-  """class t test get json"""
-  @patch('utils.requests.get')
-  def test_get_json(self, mock_get):
-    """test for get_json"""
-    test_data = [
-          ("http://example.com", {"payload": True}),
-          ("http://holberton.io", {"payload": False})
+    """class to test get_json"""
+
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        """test for get_json"""
+        test_data = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
         ]
-    for test_url, test_payload in test_data:
-      mock_response = Mock()
-      mock_response.json.return_value = test_payload
-      mock_get.return_value = mock_response
-            
-      
-      result = get_json(test_url)
-      mock_get.assert_called_once_with(test_url)
-      self.assertEqual(result, test_payload)
+        for test_url, test_payload in test_data:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
